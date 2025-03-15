df0497ea6c3ce72b05ff16eaae5e29396b98767b
Lv Yonghuan <1914128278@qq.com>
2025-03-15 16:23:29 +0800

    Adjust the plugin structure

--- uplugin/model.go
+++ uplugin/model.go
@@ -1,26 +1,24 @@
 package uplugin
 
 type Plugin struct {
-	Name        string `json:"name"`        //Name of the plugin
-	Description string `json:"description"` //Description of the plugin
-	Version     string `json:"version"`     //Version of the plugin
-	Author      string `json:"author"`      //Author of the plugin
-	Nodes       []Node `json:"nodes"`       //Plugin node metadata
+	Name        string          `json:"name"`        //Name of the plugin
+	Description string          `json:"description"` //Description of the plugin
+	Version     string          `json:"version"`     //Version of the plugin
+	Author      string          `json:"author"`      //Author of the plugin
+	Nodes       map[string]Node `json:"nodes"`       //Plugin node metadata. Key is the node name, value is the node metadata
 }
 
 type Node struct {
-	Name    string `json:"name"`     //Name of the node
-	Info    string `json:"info"`     //Description of the node
-	IsBegin bool   `json:"is_begin"` //begin node will be the first node to run
-	Input   []Port `json:"input"`
-	Output  []Port `json:"output"`
-	Params  []Port `json:"params"`
+	Info    string          `json:"info"`     //Description of the node
+	IsBegin bool            `json:"is_begin"` //begin node will be the first node to run
+	Input   map[string]Port `json:"input"`    //Input port metadata. Key is the port name, value is the port metadata
+	Output  map[string]Port `json:"output"`   //Output port metadata. Key is the port name, value is the port metadata
+	Params  map[string]Port `json:"params"`   //Parameter port metadata. Key is the port name, value is the port metadata
 }
 
 // Port represents input, output, or parameter port information
 type Port struct {
 	Attribute string `json:"attribute"` //If the properties are consistent, the two ports can be connected
-	Name      string `json:"name"`      //Name of the port. Will be displayed in the front end
 	Type      string `json:"type"`      //Type of the port. Will be displayed in the front end
 	Info      string `json:"info"`      //Description of the port
 
---
+++ uplugin/model_test.go
@@ -0,0 +1,69 @@
+package uplugin_test
+
+import (
+	"testing"
+
+	"github.com/lvyonghuan/Ubik-Util/uconfig"
+	"github.com/lvyonghuan/Ubik-Util/uplugin"
+)
+
+func TestReadModel(t *testing.T) {
+	var plugin uplugin.Plugin
+	err := uconfig.Read("./example-info.json", &plugin)
+	if err != nil {
+		t.Fatalf("Read function failed: %v", err)
+	}
+
+	// Verify basic plugin information
+	if plugin.Name != "ExamplePlugin" {
+		t.Errorf("Expected plugin name to be 'ExamplePlugin', got '%s'", plugin.Name)
+	}
+	if plugin.Version != "1.0.0" {
+		t.Errorf("Expected plugin version to be '1.0.0', got '%s'", plugin.Version)
+	}
+	if plugin.Author != "Ubik" {
+		t.Errorf("Expected plugin author to be 'Ubik', got '%s'", plugin.Author)
+	}
+
+	// Check nodes count
+	if len(plugin.Nodes) != 3 {
+		t.Errorf("Expected 3 nodes, got %d", len(plugin.Nodes))
+	}
+
+	// Verify StartNode properties
+	startNode, exists := plugin.Nodes["StartNode"]
+	if !exists {
+		t.Fatal("Expected 'StartNode' to exist in nodes")
+	}
+	if !startNode.IsBegin {
+		t.Error("Expected StartNode to be a begin node")
+	}
+	if len(startNode.Output) != 2 {
+		t.Errorf("Expected StartNode to have 2 outputs, got %d", len(startNode.Output))
+	}
+
+	// Verify ProcessNode properties
+	processNode, exists := plugin.Nodes["ProcessNode"]
+	if !exists {
+		t.Fatal("Expected 'ProcessNode' to exist in nodes")
+	}
+	if processNode.IsBegin {
+		t.Error("Expected ProcessNode to not be a begin node")
+	}
+
+	// Check streaming property
+	if !processNode.Output["processedData"].Streaming {
+		t.Error("Expected ProcessNode's processedData output to be streaming")
+	}
+
+	// Verify OutputNode parameters
+	outputNode, exists := plugin.Nodes["OutputNode"]
+	if !exists {
+		t.Fatal("Expected 'OutputNode' to exist in nodes")
+	}
+	if len(outputNode.Params) != 2 {
+		t.Errorf("Expected OutputNode to have 2 params, got %d", len(outputNode.Params))
+	}
+
+	t.Log(plugin)
+}
